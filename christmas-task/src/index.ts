8bbeb09dbe71380eb965b7d5b4d8c27264d17734
Marie Serykh <marie.serykh@gmail.com>
2021-12-15 23:59:56 +0100

    feat: create main file and implement generating list of toys

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,31 +1,14 @@
-import './global.css'
+import "./global.css";
+import data from "./assets/data/data";
+import { IToy } from "./app/utils/IToy";
+import "./app/Sorting";
 
-const API_URL: string = './public/data.json';
-
-interface IToy {
-  num: string,
-  name: string,
-  count: string,
-  year: string,
-  shape: string,
-  favorite: boolean,
-  color: string,
-  size: string
-}
-
-interface IFilter {
-  
-}
+const application = document.querySelector(".app") as HTMLElement;
+export const resultList = document.querySelector(".results__list") as HTMLElement;
 
 enum FavoriteText {
   Yes = "Да",
   No = "Нет"
-}
-
-async function getToysList(url: string): Promise<void> {
-  const res = await fetch(url);
-  const data = await res.json();
-  showToys(data);
 }
 
 function getFavoriteText(value: boolean): string {
@@ -37,24 +20,60 @@ function getFavoriteText(value: boolean): string {
   }
 }
 
-function showToys(data: [IToy]):void {
-  const page = document.getElementById("data") as HTMLElement;
 
-  data.forEach((toy: IToy) => {
-    const toyEl = document.createElement("div") as HTMLElement;
+export function showToys(data: IToy[]): void {
+  data.map((toy: IToy) => {
+    const toyEl = document.createElement("li") as HTMLElement;
+    toyEl.classList.add("results__item", "toy");
     toyEl.innerHTML = `
-      <h3>${toy.name}</h3>
-      <p>${toy.year}</p>
-      <p>${toy.shape}</p>
-      <p>${toy.color}</p>
-      <p>${toy.size}</p>
-      <p>${getFavoriteText(toy.favorite)}</p>
-      <img src="./assets/toys/${toy.num.toString()}.webp" alt="${toy.name}">
-      <hr>
+      <h2 class="toy__title">${toy.name}</h2>
+      <img class="toy__image" src="./assets/toys/${toy.num.toString()}.webp" alt="Сосулька красная">
+      <div class="toy__description">
+        <p class="toy__qty">Количество: <span>${toy.count}</span></p>
+        <p class="toy__year">Год покупки: <span>${toy.year}</span></p>
+        <p class="toy__shape">Форма: <span>${toy.shape}</span></p>
+        <p class="toy__color">Цвет: <span>${toy.color}</span></p>
+        <p class="toy__size">Размер: <span>${toy.size}</span></p>
+        <p class="toy__favorite">Любимая: <span>${getFavoriteText(toy.favorite)}</span></p>
+      </div>
+      <div class="toys__ribbon">
+      </div>
     `;
-    page.appendChild(toyEl);
+    resultList.appendChild(toyEl);
   });
 }
 
+showToys(data);
 
-getToysList(API_URL);
+
+import "./app/Filters";
+import "./app/Selected";
+
+/*import { App } from "./app/App";
+import { ToysList } from "./app/components/ToysList/ToysList";
+import { AppStore } from "./app/components/Subscribe/AppStore";*/
+
+/*const root = document.querySelector(".app") as HTMLElement; 
+
+if (!root) {
+  throw new Error("Something went wrong, body is undefined")
+}
+
+const app: App = new App();
+root.innerHTML = app.render();*/
+
+
+/*
+const END_POINT: string = "./public/data.json";
+const getToysList = async function (url: string){
+  const res = await fetch(url);
+  const data: IToy[] = await res.json();
+  if (!res.ok) throw new Error(res.statusText);
+
+  showToys(data);
+  const result = data.slice();
+
+  return result;
+}
+
+const toysData = getToysList(END_POINT);*/
