37820e526305b31ec87aef28b7ced7b76c306518
Marie Serykh <marie.serykh@gmail.com>
2021-12-20 01:57:33 +0100

    feat: implement sorting toys by name and year

--- christmas-task/src/app/components/Sorting.ts
+++ christmas-task/src/app/components/Sorting.ts
@@ -1,4 +1,6 @@
-import { render } from "./Render"
+import data from "../../assets/data/data";
+import { render } from "./Render";
+import { renderToys } from "./RenderToys";
 
 export const sorting = () => {
   const sortingContainer = document.querySelector(".filters__sorting");
@@ -11,4 +13,55 @@ export const sorting = () => {
 </select>`;
 
   render(sortingContainer, controlsComponent());
+  addEventListeners();
+};
+
+const addEventListeners = () => {
+  const filterSelect = document.querySelector(".filters__select") as HTMLElement;
+  filterSelect.addEventListener("change", (e: Event): void => { onSortValueChange(e) });
+};
+
+
+const onSortValueChange = (e: Event) => {
+  const target = e.target as HTMLSelectElement;
+
+  if (target.value === "default_sorting") {
+    renderToys(data);
+  }
+
+  if (target.value === "name-asc_sorting") {
+    const data = sortByNameAsc();
+    renderToys(data);
+  }
+
+  if (target.value === "name-desc_sorting") {
+    const data = sortByNameDesc();
+    renderToys(data);
+  }
+
+  if (target.value === "year-asc_sorting") {
+    const data = sortByYearAsc();
+    renderToys(data);
+  }
+
+  if (target.value === "year-desc_sorting") {
+    const data = sortByYearDesc();
+    renderToys(data);
+  }
+};
+
+const sortByYearDesc = () => {
+  return data.slice().sort((toy1, toy2) => +toy1.year - +toy2.year);
+};
+
+const sortByYearAsc = () => {
+  return data.slice().sort((toy1, toy2) => +toy2.year - +toy1.year);
+};
+
+const sortByNameAsc = () => {
+  return data.slice().sort((toy1, toy2) => (toy1.name.toLowerCase() > toy2.name.toLowerCase()) ? 1 : (toy2.name.toLowerCase() > toy1.name.toLowerCase()) ? - 1 : 0);
+};
+
+const sortByNameDesc = () => {
+  return data.slice().sort((toy1, toy2) => (toy1.name.toLowerCase() > toy2.name.toLowerCase()) ? -1 : (toy2.name.toLowerCase() > toy1.name.toLowerCase()) ? 1 : 0);
 };
