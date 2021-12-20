8e809f3082fc7e6627a031c988b26efd3fa15e73
Marie Serykh <marie.serykh@gmail.com>
2021-12-20 01:33:24 +0100

    feat: create range slider by years

--- christmas-task/src/app/components/Ranges.ts
+++ christmas-task/src/app/components/Ranges.ts
@@ -1,23 +1,42 @@
 import { render } from "./Render"
-import { sliderRanges } from "../SliderRange";
+import { sliderRanges } from "../utils/SliderRange";
 
 export const ranges = () => {
   const rangesContainer = document.querySelector(".filters__ranges");
   const rangesComponent = () => `<fieldset class="filters__value filters__value--range">
   <legend class="filters__name">Количество экземпляров:</legend>
   <div class="fieldset__inner">
-    <div class="range-slider range-slider--year">
+    <div class="range-slider range-slider--qty">
       <div class="slider">
-        <div class="output left">3</div>
-        <input class="filters__range filters__range--left" type="range" id="input-left" min="1" max="12" value="3">
-        <input class="filters__range filters__range--right" type="range" id="input-right" min="1" max="12" value="9">
+        <div class="output left">1</div>
+        <input class="filters__range filters__range--left" type="range" id="input-left" min="1" max="12" value="1" step="1">
+        <input class="filters__range filters__range--right" type="range" id="input-right" min="1" max="12" value="12" step="1">
         <div class="slider__inner">
           <div class="track"></div>
           <div class="range"></div>
           <div class="thumb left"></div>
           <div class="thumb right"></div>
         </div>
-        <div class="output right">9</div>
+        <div class="output right">12</div>
+      </div>
+    </div>
+  </div>
+  </fieldset>
+  <fieldset class="filters__value filters__value--range">
+  <legend class="filters__name">Год приобретения:</legend>
+  <div class="fieldset__inner">
+    <div class="range-slider range-slider--year">
+      <div class="slider">
+        <div class="output left">1940</div>
+        <input class="filters__range filters__range--left" type="range" id="input-left" min="1940" max="2020" value="1940" step="1">
+        <input class="filters__range filters__range--right" type="range" id="input-right" min="1940" max="2020" value="2020" step="1">
+        <div class="slider__inner">
+          <div class="track"></div>
+          <div class="range"></div>
+          <div class="thumb left"></div>
+          <div class="thumb right"></div>
+        </div>
+        <div class="output right">2020</div>
       </div>
     </div>
   </div>
