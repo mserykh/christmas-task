5d8eb81080c46dfb50cb496b7e10cccc66abf34d
Marie Serykh <marie.serykh@gmail.com>
2021-12-27 18:05:30 +0100

    feat: add audio toggle

---
+++ christmas-task/src/app/components/Tree/Audio.ts
@@ -0,0 +1,51 @@
+import { render } from "../../utils/Render";
+import { TemplateFunction } from "../../utils/Types";
+
+export const letTheMusicPlay = (): void => {
+  const audioControlTemplate: TemplateFunction = () => `<div class="audio-control control"></div>`;
+
+  const container = document.querySelector<HTMLElement>(".settings__snow-audio");
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const audio = new Audio() ;
+  audio.src = "./assets/audio/greendolphinstreet.mp3";
+  let isPlaying = false;
+
+  if (container !== null) {
+    render(container, audioControlTemplate());
+  }
+
+  const audioToggle = (e) => {
+    if (isPlaying) {
+      audio.currentTime = 0;
+      audio.pause();
+      isPlaying = false;
+      toggleIcon(e);
+    }
+    else {
+      audio.play();
+      isPlaying = true;
+      audio.loop = true;
+      audio.volume = 0.5;
+      toggleIcon(e);
+    }
+  }
+
+  const toggleIcon = (e) => {
+    if (!isPlaying) {
+      (e.target as HTMLElement).classList.remove("control--active");
+    }
+    else {
+      (e.target as HTMLElement).classList.add("control--active");
+    }
+  }
+
+  const onAudioClick = (e) => {
+    audioToggle(e);
+  };
+
+  const audioControl = document.querySelector(".audio-control");
+  if (audioControl !== null) {
+    audioControl.addEventListener("click", (e: Event) => onAudioClick(e));
+  }
+};
