1bab728665692ad5c61a2053f037ecfb8f7658d1
Marie Serykh <marie.serykh@gmail.com>
2021-12-27 18:08:27 +0100

    feat: create container for a page with tree

---
+++ christmas-task/src/app/components/TreePage.ts
@@ -0,0 +1,9 @@
+import { TemplateFunction } from "../utils/Types";
+import { settingsLeftContainer } from "./Tree/SettingsLeftContainer";
+
+export const treePage: TemplateFunction = () => {
+    return `<div class="container--tree container">
+  </div>`;
+};
+
+
